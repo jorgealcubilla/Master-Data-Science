85f44497bf8f65346eb9191380a9037560bec180
Jorge Alcubilla <jaasc27@gmail.com>
2018-04-04 10:06:39 +0200

    'Practice# 2, Section# 1: adding comments'

--- Shell/Master_class_03v7_pag20_01.sh
+++ Shell/Master_class_03v7_pag20_01.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Processing and filtering: Practice#2, section#1
+# Go to ~/Data/opentraveldata
+# 1. Use grep to extract all 7x7 or 3xx (where x can be any number) airplane models from optd_aircraft.csv.
+
 cut -d "^" -f 3 /home/dsc/Data/opentraveldata/optd_aircraft.csv | grep -P '(?<!\d)7\d7(?!\d)|(?<!\d)3\d\d(?!\d)'
