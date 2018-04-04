8c4d8dbea8b66f5363ce2f6d02342a0207787638
Jorge Alcubilla <jaasc27@gmail.com>
2018-04-04 11:22:58 +0200

    new attempt for section#4

--- Shell/Master_class_03v7_pag20_04.sh
+++ Shell/Master_class_03v7_pag20_04.sh
@@ -5,6 +5,6 @@
 # 4. Find all files with txt extension that have word “Science” (case unsensitive) inside the content. Print file
 # path and the line contaning the (S/s)cience word.
 
-find -type f -name "*.txt" -exec grep -w -H -i "science" {} \; 
+find "/" -type f -name "*.txt" -exec grep -w -H -i "science" {} \; 
 
 
